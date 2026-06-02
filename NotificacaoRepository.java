fa02a6fcd8ba9e4ac4799ad7072a7e64fe629b92
Miguel-JW <conteudojurassicojw@gmail.com>
2026-06-02 10:49:19 -0300

    Create NotificacaoRepository.java

---
+++ NotificacaoRepository.java
@@ -0,0 +1,6 @@
+package com.loja.notificacao.repository;
+
+import com.loja.notificacao.entity.Notificacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificacaoRepository extends JpaRepository<Notificacao, Long> {}
